e02f67761fe418d0d7127a1737118386c2a42f9b
Tuubby <mmantas.kazlauskas@gmail.com>
2025-06-18 11:26:31 +0300

    Init commit

---
+++ Main.lua
@@ -0,0 +1,9 @@
+---@class ChattyLittleNpc_Dragonflight_voiceovers
+local ChattyLittleNpc_Dragonflight_voiceovers = LibStub("AceAddon-3.0"):NewAddon("ChattyLittleNpc_Dragonflight_voiceovers")
+
+ChattyLittleNpc_Dragonflight_voiceovers.Metadata = {
+    ContainsQuestVoiceovers = true,
+    ContainsGossipVoiceovers = true,
+    ContainsGameObjectVoiceovers = true,
+    ContainsItemVoiceovers = true
+}
